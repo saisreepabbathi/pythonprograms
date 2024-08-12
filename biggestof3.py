53e62177eaa38c694734d2b9dd0d11ada6afdaa2
saisreepabbathi <pabbathisaisree@gmail.com>
2024-08-12 21:09:47 +0530

    Add files via upload

---
+++ biggestof3.py
@@ -0,0 +1,10 @@
+a=int(input(" enter first num:"))
+b=int(input("enter second num:"))
+c=int(input("enter third num:"))
+if a>b and a>c:
+    print("a is largest num")
+elif b>a and b>c:
+    print("b is largest num")
+else:
+    print("c is largest num")
+    
